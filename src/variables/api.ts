f896ef3742a42f121a483a0bf7d6720a9a1bb810
Kiadtisak Lowongsa <kanlowongsa20@gmail.com>
2022-02-20 10:50:58 +0700

    define variables and types

---
+++ src/variables/api.ts
@@ -0,0 +1,6 @@
+const movieDatabaseAPI = process.env.REACT_APP_MOVIE_DATABASE_API;
+const movieImageURL = process.env.REACT_APP_MOVIE_IMAGE_URL;
+const ORIGINAL_SIZE = `${movieImageURL}/original/`;
+const W500_SIZE = `${movieImageURL}/w500/`;
+
+export { ORIGINAL_SIZE, W500_SIZE, movieDatabaseAPI };
