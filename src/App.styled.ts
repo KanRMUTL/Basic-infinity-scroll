96a12a1f910d16d6fe8f5fc435d1a171fbb477fa
Kiadtisak Lowongsa <kanlowongsa20@gmail.com>
2022-02-20 11:46:32 +0700

    make infinit scroll

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -1,10 +1,16 @@
 import styled from "styled-components";
 
-const Wrapper = styled.div`
+const Wrapper = styled.div``;
+
+const Content = styled.div`
   display: flex;
   justify-content: space-around;
   flex-wrap: wrap;
   row-gap: 1rem;
+  width: 800px;
+  height: 800px;
+  margin: 0 auto;
+  overflow: auto;
 `;
 
-export { Wrapper };
+export { Wrapper, Content };
--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useEffect, UIEvent } from "react";
 import getMovies from "client/getMovies";
 import { useState } from "react";
 import { Movie } from "types/Movie";
 import MovieItem from "MovieItem/MovieItem";
-import { Wrapper } from "App.styled";
-function App() {
+import { Wrapper, Content } from "App.styled";
+
+const App = () => {
   const [page, setPage] = useState(1);
   const [movies, setMovies] = useState<Movie[]>([]);
 
   useEffect(() => {
     const fetchMovies = async () => {
-      setMovies(await getMovies(page));
+      const result = await getMovies(page);
+      setMovies((pre) => [...pre, ...result]);
     };
     fetchMovies();
   }, [page]);
 
+  const handleScroll = (event: UIEvent<HTMLDivElement>) => {
+    const { scrollTop, clientHeight, scrollHeight } = event.currentTarget;
+    if (scrollHeight - scrollTop === clientHeight) {
+      setPage((prev) => prev + 1);
+    }
+  };
+
   return (
     <Wrapper>
-      {movies.map((movie) => (
-        <MovieItem key={movie.id} movie={movie} />
-      ))}
+      <Content onScroll={handleScroll}>
+        {movies.map((movie) => (
+          <MovieItem key={movie.id} movie={movie} />
+        ))}
+      </Content>
     </Wrapper>
   );
-}
+};
 
 export default App;
